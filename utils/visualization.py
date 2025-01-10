e1b7d787f129a34ec62fe4498275596de45e60d2
alfredowhj <alfredowijaya@gmail.com>
2025-01-10 02:31:39 +0100

    add num of weights viz

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 from typing import Literal, List
 import numpy as np
+import torch
+from torchvision import models
+from models.pruning import Pruning
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = models.mobilenet_v2(weights='MobileNet_V2_Weights.IMAGENET1K_V1').to(DEVICE).eval()
 
 # Benchmark
 acc_top1 = 71.87
@@ -108,13 +114,12 @@ def compare_test_metrics(datas, labels: List[str]) -> None:
     width = 0.5 / len(datas)
 
     for idx, data in enumerate(datas):
-        plt.bar(x + idx * width, np.array(data["top1"]) * 100, width, label=labels[idx])
+        plt.plot(datas[0]["ratio"], np.array(data["top1"]) * 100, label=labels[idx], marker="o", linewidth=1)
 
-    plt.xticks(x + (width * (len(datas) - 1)) / 2, datas[0]["ratio"])
     plt.xlabel("Pruning Ratio")
     plt.ylabel("Acc@1 in %")
     plt.ylim([0, 100])
-    plt.xlim([-0.2, len(x) - 0.5])
+    plt.xlim([0.0, 1.0])
     plt.grid(ls="--", axis="y", alpha=0.5)
     plt.legend()
 
@@ -125,15 +130,44 @@ def compare_test_metrics(datas, labels: List[str]) -> None:
     plt.plot(np.linspace(-1, len(x), 10), np.ones(10) * acc_top5, color="red", linestyle="dashed", linewidth=1)
 
     for idx, data in enumerate(datas):
-        plt.bar(x + idx * width, np.array(data["top5"]) * 100, width, label=labels[idx])
+        plt.plot(datas[0]["ratio"], np.array(data["top5"]) * 100, label=labels[idx], marker="o", linewidth=1)
 
-    plt.xticks(x + (width * (len(datas) - 1)) / 2, datas[0]["ratio"])
     plt.xlabel("Pruning Ratio")
     plt.ylabel("Acc@5 in %")
     plt.ylim([0, 100])
-    plt.xlim([-0.2, len(x) - 0.5])
+    plt.xlim([0.0, 1.0])
 
     plt.grid(ls="--", axis="y", alpha=0.5)
     plt.legend()
 
     plt.show()
+
+
+def plot_cp(data, label, original=False):
+    if original:
+        plt.plot(range(len(data.keys())), data.values(), label=label, c="red", linestyle="--", alpha=1.0, linewidth=0.5)
+    else:
+        plt.plot(range(len(data.keys())), data.values(), label=label)
+    plt.xticks(range(len(data.keys())))
+
+
+def iter_ratios(pruned_layers, pruning_ratios, pruning_type, level="global"):
+    for r in pruning_ratios:
+        model = models.mobilenet_v2(weights='MobileNet_V2_Weights.IMAGENET1K_V1').to(DEVICE).eval()
+        pruning = Pruning(model, DEVICE)
+        if pruning_type == 'batchnorm':
+            model = pruning.scaling_based_pruning(batch_norms=pruned_layers, pruning_ratio=r, level=level,
+                                                  scale_threshold=False)
+        elif pruning_type == 'magnitude':
+            model = pruning.magnitude_based_pruning(conv_layers=pruned_layers, pruning_ratio=r, level=level,
+                                                    scale_threshold=False)
+
+        data, total = pruning.count_parameters()
+
+        plot_cp(data, label=f"ratio: {r}, {level}, total: {total / 1e6:.3}M")
+
+    plt.yscale("log")
+    plt.ylabel("#Parameter")
+    plt.xlabel("Conv Layer")
+    plt.grid(axis="y", linestyle="--", alpha=0.5)
+    plt.legend()
